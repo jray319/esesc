3817a0b7af1b6655aedf94d324943e3644784776
szzhang <szzhang@mit.edu>
2015-04-09 21:58:08 -0400

    add run_splash function to benchmark.py

--- bins/benchmark.py
+++ bins/benchmark.py
@@ -10,86 +10,6 @@ bench_root_dir = '/home/szzhang/local-proj/mem-model/simulation/program'
 # params to run launcher
 launch_param = {}
 
-'''
-launch_param['blackscholes'] = {
-		'dev'    : '__THREAD_NUM__ in_16.txt blackscholes_dev.out'     ,
-		'small'  : '__THREAD_NUM__ in_4K.txt blackscholes_small.out'   ,
-		'medium' : '__THREAD_NUM__ in_16K.txt blackscholes_medium.out' ,
-		'large'  : '__THREAD_NUM__ in_64K.txt blackscholes_large.out'
-		}
-
-launch_param['bodytrack'] = { # we use posix thread model (2)
-		'dev'    : 'sequenceB_1 4 1 100 3 2 __THREAD_NUM__'  ,
-		'small'  : 'sequenceB_1 4 1 1000 5 2 __THREAD_NUM__' ,
-		'medium' : 'sequenceB_2 4 2 2000 5 2 __THREAD_NUM__' ,
-		'large'  : 'sequenceB_4 4 4 4000 5 2 __THREAD_NUM__'
-		}
-
-launch_param['facesim'] = {
-		'dev'    : '-timing -threads __THREAD_NUM__' ,
-		'small'  : '-timing -threads __THREAD_NUM__' ,
-		'medium' : '-timing -threads __THREAD_NUM__' ,
-		'large'  : '-timing -threads __THREAD_NUM__' ,
-		}
-
-launch_param['ferret'] = {
-		'dev'    : 'corel lsh queries 5 5 __THREAD_NUM__ ferret_dev.out'      ,
-		'small'  : 'corel lsh queries 10 20 __THREAD_NUM__ ferret_small.out'  ,
-		'medium' : 'corel lsh queries 10 20 __THREAD_NUM__ ferret_medium.out' ,
-		'large'  : 'corel lsh queries 10 20 __THREAD_NUM__ ferret_large.out'
-		}
-
-launch_param['fluidanimate'] = {
-		'dev'    : '__THREAD_NUM__ 3 in_15K.fluid fluidanimate_dev.out'     ,
-		'small'  : '__THREAD_NUM__ 5 in_35K.fluid fluidanimate_small.out'   ,
-		'medium' : '__THREAD_NUM__ 5 in_100K.fluid fluidanimate_medium.out' ,
-		'large'  : '__THREAD_NUM__ 5 in_300K.fluid fluidanimate_large.out'
-		}
-
-launch_param['swaptions'] = {
-		'dev'    : '-ns 16 -sm 50 -nt __THREAD_NUM__'     , # change 3 to 16 to enable 8 threads
-		'small'  : '-ns 16 -sm 5000 -nt __THREAD_NUM__'  ,
-		'medium' : '-ns 32 -sm 10000 -nt __THREAD_NUM__' ,
-		'large'  : '-ns 64 -sm 20000 -nt __THREAD_NUM__'
-		}
-
-launch_param['x264'] = {
-		'dev'    : ('--quiet --qp 20 --partitions b8x8,i4x4 --ref 5 --direct auto --b-pyramid ' +
-					'--weightb --mixed-refs --no-fast-pskip --me umh --subme 7 --analyse b8x8,i4x4 ' +
-					'--threads __THREAD_NUM__ -o x264_dev.264 eledream_64x36_3.y4m')       ,
-		'small'  : ('--quiet --qp 20 --partitions b8x8,i4x4 --ref 5 --direct auto --b-pyramid ' +
-					'--weightb --mixed-refs --no-fast-pskip --me umh --subme 7 --analyse b8x8,i4x4 ' +
-					'--threads __THREAD_NUM__ -o x264_small.264 eledream_640x360_8.y4m')   ,
-		'medium' : ('--quiet --qp 20 --partitions b8x8,i4x4 --ref 5 --direct auto --b-pyramid ' +
-					'--weightb --mixed-refs --no-fast-pskip --me umh --subme 7 --analyse b8x8,i4x4 ' +
-					'--threads __THREAD_NUM__ -o x264_medium.264 eledream_640x360_32.y4m') ,
-		'large'  : ('--quiet --qp 20 --partitions b8x8,i4x4 --ref 5 --direct auto --b-pyramid ' +
-					'--weightb --mixed-refs --no-fast-pskip --me umh --subme 7 --analyse b8x8,i4x4 ' +
-					'--threads __THREAD_NUM__ -o x264_large.264 eledream_640x360_128.y4m')
-		}
-
-launch_param['ocean'] = {
-		'dev'    : '-n258 -p__THREAD_NUM__ -e1e-07 -r20000 -t28800'  ,
-		'small'  : '-n514 -p__THREAD_NUM__ -e1e-07 -r20000 -t28800'  ,
-		'medium' : '-n1026 -p__THREAD_NUM__ -e1e-07 -r20000 -t28800' ,
-		'large'  : '-n2050 -p__THREAD_NUM__ -e1e-07 -r20000 -t28800'
-		}
-
-launch_param['fft'] = {
-		'dev'    : '-m18 -p__THREAD_NUM__' ,
-		'small'  : '-m20 -p__THREAD_NUM__' ,
-		'medium' : '-m22 -p__THREAD_NUM__' ,
-		'large'  : '-m24 -p__THREAD_NUM__'
-		}
-
-launch_param['radix'] = {
-		'dev'    : '-p__THREAD_NUM__ -r4096 -n262144 -m524288'       ,
-		'small'  : '-p__THREAD_NUM__ -r4096 -n4194304 -m2147483647'  ,
-		'medium' : '-p__THREAD_NUM__ -r4096 -n16777216 -m2147483647' ,
-		'large'  : '-p__THREAD_NUM__ -r4096 -n67108864 -m2147483647' 
-		}
-'''
-
 launch_param['canneal'] = {
 		'dev'    : '__THREAD_NUM__ 100 300 100.nets 2'         ,
 		'small'  : '__THREAD_NUM__ 10000 2000 100000.nets 32'  ,
@@ -190,6 +110,107 @@ parsec3_param['x264'] = {
 		'thread' : lambda core : core - 1
 		}
 
+# parameter to run SPLASH2x
+splash_param = {}
+
+splash_param['barnes'] = {
+		'dev'      : '__THREAD_NUM__ < input.template' ,
+		'small'    : '__THREAD_NUM__ < input.template' ,
+		'medium'   : '__THREAD_NUM__ < input.template' ,
+		'large'    : '__THREAD_NUM__ < input.template' ,
+		'thread'   : lambda core : core ,
+		'template' : True
+		}
+
+splash_param['fmm'] = {
+		'dev'      : '__THREAD_NUM__ < input.template' ,
+		'small'    : '__THREAD_NUM__ < input.template' ,
+		'medium'   : '__THREAD_NUM__ < input.template' ,
+		'large'    : '__THREAD_NUM__ < input.template' ,
+		'thread'   : lambda core : core ,
+		'template' : True
+		}
+
+splash_param['ocean_cp'] = {
+		'dev'      : '-n258 -p__THREAD_NUM__ -e1e-07 -r20000 -t28800'  ,
+		'small'    : '-n514 -p__THREAD_NUM__ -e1e-07 -r20000 -t28800'  ,
+		'medium'   : '-n1026 -p__THREAD_NUM__ -e1e-07 -r20000 -t28800' ,
+		'large'    : '-n2050 -p__THREAD_NUM__ -e1e-07 -r20000 -t28800' ,
+		'thread'   : lambda core : core ,
+		'template' : False
+		}
+
+splash_param['radiosity'] = {
+		'dev'      : '-bf 1.5e-1 -batch -room -p __THREAD_NUM__' ,
+		'small'    : '-bf 1.5e-1 -batch -room -p __THREAD_NUM__' ,
+		'medium'   : '-bf 1.5e-2 -batch -room -p __THREAD_NUM__' ,
+		'large'    : '-bf 1.5e-3 -batch -room -p __THREAD_NUM__' ,
+		'thread'   : lambda core : core ,
+		'template' : False
+		}
+
+splash_param['raytrace'] = {
+		'dev'      : '-s -p__THREAD_NUM__ -a4 teapot.env' ,
+		'small'    : '-s -p__THREAD_NUM__ -a8 teapot.env' ,
+		'medium'   : '-s -p__THREAD_NUM__ -a2 balls4.env' ,
+		'large'    : '-s -p__THREAD_NUM__ -a8 balls4.env' ,
+		'thread'   : lambda core : core ,
+		'template' : False
+		}
+
+splash_param['water_nsquared'] = {
+		'dev'      : '__THREAD_NUM__ < input.template' ,
+		'small'    : '__THREAD_NUM__ < input.template' ,
+		'medium'   : '__THREAD_NUM__ < input.template' ,
+		'large'    : '__THREAD_NUM__ < input.template' ,
+		'thread'   : lambda core : core ,
+		'template' : True
+		}
+
+splash_param['water_spatial'] = {
+		'dev'      : '__THREAD_NUM__ < input.template' ,
+		'small'    : '__THREAD_NUM__ < input.template' ,
+		'medium'   : '__THREAD_NUM__ < input.template' ,
+		'large'    : '__THREAD_NUM__ < input.template' ,
+		'thread'   : lambda core : core ,
+		'template' : True
+		}
+
+splash_param['cholesky'] = {
+		'dev'      : 'tk14.O' ,
+		'small'    : 'tk29.O' ,
+		'medium'   : 'tk29.O' ,
+		'large'    : 'tk29.O' ,
+		'thread'   : lambda core : core ,
+		'template' : False
+		}
+
+splash_param['fft'] = {
+		'dev'      : '-m18 -p __THREAD_NUM__' ,
+		'small'    : '-m20 -p __THREAD_NUM__' ,
+		'medium'   : '-m22 -p __THREAD_NUM__' ,
+		'large'    : '-m22 -p __THREAD_NUM__' ,
+		'thread'   : lambda core : core ,
+		'template' : False
+		}
+
+splash_param['lu_cb'] = {
+		'dev'      : '-p__THREAD_NUM__ -n512 -b16'  ,
+		'small'    : '-p__THREAD_NUM__ -n512 -b16'  ,
+		'medium'   : '-p__THREAD_NUM__ -n1024 -b16' ,
+		'large'    : '-p__THREAD_NUM__ -n2048 -b16' ,
+		'thread'   : lambda core : core ,
+		'template' : False
+		}
+
+splash_param['radix'] = {
+		'dev'      : '-p__THREAD_NUM__ -r4096 -n262144 -m524288'       ,
+		'small'    : '-p__THREAD_NUM__ -r4096 -n4194304 -m2147483647'  ,
+		'medium'   : '-p__THREAD_NUM__ -r4096 -n16777216 -m2147483647' ,
+		'large'    : '-p__THREAD_NUM__ -r4096 -n67108864 -m2147483647' ,
+		'thread'   : lambda core : core ,
+		'template' : False
+		}
 
 # helper to remove files in current dir
 def remove_here(file_list):
@@ -343,6 +364,67 @@ def run_parsec3(name, size, mem_model, core_num, thread_num = 0, comment = ""):
 	# remove input files & exe
 	remove_here(all_inputs)
 	os.remove(name)
+
+	# change report file mode
+	shell_cmd = 'chmod 644 esesc_' + report_file + '.*'
+	os.system(shell_cmd)
+
+	return True
+####
+
+# subroutine to runi SPLASH2X benchmarks
+def run_splash(name, size, mem_model, core_num, thread_num = 0):
+	# check we have param for benchmark
+	if name not in splash_param:
+		print(name + " doesn't have parameters!")
+		return False
+	elif size not in splash_param[name]:
+		print(name + " doesn't have parameter for size " + size)
+		return False
+
+	# copy exe
+	if os.path.isfile(name): # remove existing one
+		os.remove(name)
+	exe_path = os.path.join(bench_root_dir, 'splash', name, name)
+	if not os.path.isfile(exe_path):
+		print(exe_path + " doesn't exist!")
+		return False
+	shutil.copy(exe_path, '.')
+
+	# copy input files
+	input_dir = os.path.join(bench_root_dir, 'splash', name, size)
+	all_inputs = copy_here(input_dir)
+
+	# change esesc.conf
+	if thread_num == 0: # thread_num is at default, we use func in splash_param to calculate it
+		thread_num = splash_param[name]['thread'](core_num)
+	cpu_max_id = str(core_num - 1)
+	bench_cmd = name + ' ' + splash_param[name][size]
+	bench_cmd = re.sub('__THREAD_NUM__', str(thread_num), bench_cmd)
+	report_file = 'splash_' + name + '_' + mem_model + '_' + size + '_c' + str(core_num) + '_t' + str(thread_num)
+
+	shell_cmd = (
+			"sed 's/__CPU_MAX_ID__/" + cpu_max_id + "/g' esesc.conf.template | " + 
+			"sed 's/__MEMORY_MODEL__/" + mem_model + "/g' | " +
+			"sed 's/__BENCH_NAME__/" + bench_cmd + "/g' | " +
+			"sed 's/__REPORT_FILE__/" + report_file + "/g' > esesc.conf"
+			)
+	print(shell_cmd)
+	os.system(shell_cmd)
+
+	# some benchmarks needs to change input.template
+	shell_cmd = "sed -i 's/NUMPROCS/" + str(thread_num) + "/g' input.template"
+	os.system(shell_cmd)
+	
+	# run
+	log_file = report_file + '.log'
+	shell_cmd = '../main/esesc 2>&1 | tee ' + log_file
+	print(shell_cmd)
+	os.system(shell_cmd)
+
+	# remove input files & exe
+	#remove_here(all_inputs)
+	#os.remove(name)
 
 	# change report file mode
 	shell_cmd = 'chmod 644 esesc_' + report_file + '.*'
